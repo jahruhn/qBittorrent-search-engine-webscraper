2a92f89faffc51f83329b99b8d0e8f56c069491b
jahruhn <48630231+jahruhn@users.noreply.github.com>
2021-03-05 10:40:30 +0100

    Update scrape-search-engines.sh

--- scrape-search-engines.sh
+++ scrape-search-engines.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 lynx -dump https://github.com/qbittorrent/search-plugins/wiki/Unofficial-search-plugins#plugins-for-public-sites | awk '/py/{print $2}' > qbittorrent-search-engine.txt && wget -i qbittorrent-search-engine.txt
 
